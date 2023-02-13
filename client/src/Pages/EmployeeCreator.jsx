5d0cf846c3590b2e1ac5427285f71945fc46f6f8
alexsincai <alex.sincai@yahoo.co.uk>
2023-02-13 14:09:06 +0200

    reformat + slight refactor

--- client/src/Pages/EmployeeCreator.jsx
+++ client/src/Pages/EmployeeCreator.jsx
@@ -2,42 +2,45 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import EmployeeForm from "../Components/EmployeeForm";
 
-const createEmployee = (employee) => {
-  return fetch("/api/employees", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(employee),
-  }).then((res) => res.json());
+const createEmployee = async (employee) => {
+    const res = await fetch("http://localhost:8081/api/employees", {
+        method: "POST",
+        headers: {
+            "Access-Control-Allow-Origin": true,
+            "Content-Type": "application/json",
+        },
+        mode: "cors",
+        body: JSON.stringify(employee),
+    });
+    return await res.json();
 };
 
 const EmployeeCreator = () => {
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
 
-  const handleCreateEmployee = (employee) => {
-    setLoading(true);
+    const handleCreateEmployee = (employee) => {
+        setLoading(true);
 
-    createEmployee(employee)
-      .then(() => {
-        navigate("/");
-      })
-      .catch((err) => {
-        throw err;
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+        createEmployee(employee)
+            .then(() => {
+                navigate("/");
+            })
+            .catch((err) => {
+                throw err;
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
 
-  return (
-    <EmployeeForm
-      onCancel={() => navigate("/")}
-      disabled={loading}
-      onSave={handleCreateEmployee}
-    />
-  );
+    return (
+        <EmployeeForm
+            onCancel={() => navigate("/")}
+            disabled={loading}
+            onSave={handleCreateEmployee}
+        />
+    );
 };
 
 export default EmployeeCreator;
--- client/src/Pages/EmployeeUpdater.jsx
+++ client/src/Pages/EmployeeUpdater.jsx
@@ -5,65 +5,67 @@ import EmployeeForm from "../Components/EmployeeForm";
 import Loading from "../Components/Loading";
 
 const updateEmployee = (employee) => {
-  return fetch(`/api/employees/${employee._id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(employee),
-  }).then((res) => res.json());
+    return fetch(`http://localhost:8081/api/employees/${employee._id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(employee),
+    }).then((res) => res.json());
 };
 
 const fetchEmployee = (id) => {
-  return fetch(`/api/employees/${id}`).then((res) => res.json());
+    return fetch(`http://localhost:8081/api/employees/${id}`).then((res) =>
+        res.json()
+    );
 };
 
 const EmployeeUpdater = () => {
-  const { id } = useParams();
-  const navigate = useNavigate();
+    const { id } = useParams();
+    const navigate = useNavigate();
 
-  const [employee, setEmployee] = useState(null);
-  const [updateLoading, setUpdateLoading] = useState(false);
-  const [employeeLoading, setEmployeeLoading] = useState(true);
+    const [employee, setEmployee] = useState(null);
+    const [updateLoading, setUpdateLoading] = useState(false);
+    const [employeeLoading, setEmployeeLoading] = useState(true);
 
-  useEffect(() => {
-    setEmployeeLoading(true);
-    fetchEmployee(id)
-      .then((employee) => {
-        setEmployee(employee);
-        setEmployeeLoading(false);
-      })
-      .catch((error) => {
-        throw error;
-      });
-  }, [id]);
+    useEffect(() => {
+        setEmployeeLoading(true);
+        fetchEmployee(id)
+            .then((employee) => {
+                setEmployee(employee);
+                setEmployeeLoading(false);
+            })
+            .catch((error) => {
+                throw error;
+            });
+    }, [id]);
 
-  const handleUpdateEmployee = (employee) => {
-    setUpdateLoading(true);
-    updateEmployee(employee)
-      .then(() => {
-        navigate("/");
-      })
-      .catch((error) => {
-        throw error;
-      })
-      .finally(() => {
-        setUpdateLoading(false);
-      });
-  };
+    const handleUpdateEmployee = (employee) => {
+        setUpdateLoading(true);
+        updateEmployee(employee)
+            .then(() => {
+                navigate("/");
+            })
+            .catch((error) => {
+                throw error;
+            })
+            .finally(() => {
+                setUpdateLoading(false);
+            });
+    };
 
-  if (employeeLoading) {
-    return <Loading />;
-  }
+    if (employeeLoading) {
+        return <Loading />;
+    }
 
-  return (
-    <EmployeeForm
-      employee={employee}
-      onSave={handleUpdateEmployee}
-      disabled={updateLoading}
-      onCancel={() => navigate("/")}
-    />
-  );
+    return (
+        <EmployeeForm
+            employee={employee}
+            onSave={handleUpdateEmployee}
+            disabled={updateLoading}
+            onCancel={() => navigate("/")}
+        />
+    );
 };
 
 export default EmployeeUpdater;
